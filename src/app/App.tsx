634450e245083983fb70a82863d9908d0975473e
ivanvzvodnov <vzvodnov43@gmail.com>
2023-08-30 00:19:47 +0300

    13 svg loader, file loader and button component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,16 @@
-import React, { Suspense, useContext, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { Route, Routes } from 'react-router-dom'
 import "./styles/index.scss"
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from 'app/providers/ThemeProvider'
-import { Aboutpage } from 'pages/AboutPage'
-import { MainPage } from 'pages/MainPage'
 import { AppRouter } from './providers/router'
 import { Navbar } from 'widgets/Navbar'
 
 export const App = () => {
-  const {theme, toggleTheme} = useTheme()
+  const { theme } = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
       <AppRouter />
-      <button onClick={toggleTheme}>Toggle</button>
     </div>
   )
 }
