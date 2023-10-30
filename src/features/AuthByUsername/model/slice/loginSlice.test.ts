36f0fc39d1ca9640df507477475c6564fa900363
ivanvzvodnov <vzvodnov43@gmail.com>
2023-10-30 23:45:13 +0300

    36 authByUsername tests & TestAsyncThunk

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,24 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { loginByUsername } from '../services/loginByUsername/loginByUsername';
+import { LoginSchema } from '../types/loginSchema';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('loginSlice.test tests', () => {
+    test('test set username', () => {
+        const state: DeepPartial<LoginSchema> = { username: '123' };
+
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setUserName('name'),
+        )).toEqual({ username: 'name' });
+    });
+
+    test('test set password', () => {
+        const state: DeepPartial<LoginSchema> = { password: '123' };
+
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setPassword('321'),
+        )).toEqual({ password: '321' });
+    });
+});
